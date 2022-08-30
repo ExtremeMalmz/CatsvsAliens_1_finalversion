d9f05f5a4ee3b401ba23061a472831f219232709
Mr E <98057564+ExtremeMalmz@users.noreply.github.com>
2022-08-30 16:11:43 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,13 @@
+Dear group,
+
+
+Ni måste ladda ner Pygame och Pygame_Menu ramverket för att spela spelet.
+
+Detta gör ni med att ni anger "pip install pygame" och "pip install pygame_menu" i eran konsol på VisualStudioCode.
+
+Installera också ffpyplayer,pymediainfo        
+
+---
+LINK TO GIT REPOSITORY: 
+
+Regards, Management
---
+++ main.py
@@ -0,0 +1,14 @@
+from GameFiles.main_menu import the_main
+from GameFiles.main_json import create_json
+from Levels.first_boss_fight import first_boss_fight_main
+#from Levels.second_boss_fight import second_boss_fight_main
+
+if __name__ == '__main__':
+    '''
+    if the name of the file is main.py it will run, otherwise not
+    '''
+    create_json()
+    first_boss_fight_main()
+    #second_boss_fight_main()
+    the_main()
+    
